35988fd277df00a0a5f689186f7e55f3753d45d2
Jesuscruz77 <122043768@upq.edu.mx>
2025-02-18 19:43:50 -0600

    Endpoint para obtener datos
    Uso del método GET para obtener todos los datos agregados.

--- FASTAPI2/entornoTareas/main.py
+++ FASTAPI2/entornoTareas/main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI2, HTTPException
+from fastapi import FastAPI, HTTPException
 from typing import Optional
 
-app = FastAPI2(
+app = FastAPI(
     title="API de Gestión de Tareas (To-Do List)",
     description="Jesús Cruz",
     version="1.0.1"
 ) 
 
 tareas = [
-    {"id": 1, "titulo": "Estudiar para el examen", "descripcion": "Repasar los apuntes de TAI", "vencimiento": "14-02-24", "estado": "Completada"},
+    {"id": 1, "titulo": "Estudiar para el examen", "descripcion": "Repasar los apuntes de TAI", "vencimiento": "14-02-25", "estado": "Completada"},
+    {"id": 2, "titulo": "Entregar avance de Enpoint", "descripcion": "Registrar avances en GitHub", "vencimiento": "14-02-25", "estado": "Completada"},
+    {"id": 3, "titulo": "Hacer Enpoint de obtener datos", "descripcion": "Crear un endpoint con el metodo GET", "vencimiento": "16-02-25", "estado": "Pendiente"},
+    {"id": 4, "titulo": "Hacer Enpoint de agregar datos", "descripcion": "Crear un endpoint con el metodo POST", "vencimiento": "17-02-25", "estado": "Pendiente"},
+    {"id": 5, "titulo": "Hacer Enpoint de actualizar datos", "descripcion": "Crear un endpoint con el metodo PUT", "vencimiento": "18-02-25", "estado": "Pendiente"},
+    {"id": 6, "titulo": "Hacer Enpoint de eliminar datos", "descripcion": "Crear un endpoint con el metodo DELETE", "vencimiento": "19-02-25", "estado": "Pendiente"}
 ]
+
+
+
+#Endpoint para consultar todas las tareas
+@app.get("/tareas", tags=["Obtener Tareas"])
+def ConsultarTodos():
+    return {"Tareas Registradas": tareas}
